fe3907e133d66866bbf15365b4564cafcf1ee613
Son Dao <udemydaohsn@gmail.com>
2023-07-07 21:14:00 -0700

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
